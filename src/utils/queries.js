d018d4afad11ba2621e8d6a48b84d07296cb1eb4
Guillermo Campanudo <guillermo.campanudo@hotmail.com>
2022-02-05 12:44:01 +0000

    Adding the ABI for the curve pool and also adding the unusedBalances script and the totalTvl script

---
+++ src/utils/queries.js
@@ -0,0 +1,45 @@
+const { gql } = require("graphql-tag");
+
+const globalData = gql`
+  query {
+    globalDatas(first: 100) {
+      symbol
+      address
+      type
+      totalUnderlying
+      totalVolumeUSD
+    }
+  }
+`;
+
+const walletQuery = gql`
+  query {
+    smartWallets(first: 1000) {
+      owner
+      address
+    }
+  }
+`;
+
+const PAIR_DATA = (pair) => {
+  return gql`
+    query  {
+      pairs(where:{id:"${pair}"}) {
+          id
+          token0{id symbol}
+          token1{id symbol}
+          reserve0
+          reserve1
+          totalSupply
+          token0Price
+          token1Price
+      }
+    }
+  `;
+};
+
+module.exports = {
+  globalData,
+  PAIR_DATA,
+  walletQuery,
+};
